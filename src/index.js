77fef93130e9ec39fedefcb1f3e9aa4fc63994fb
Viktor Ermakov <79091602201@yandex.ru>
2023-02-21 23:54:10 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,35 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    let strMorze = '';
+    const morseValue = Object.values(MORSE_TABLE);
+    for (const char of expr) {
+        if(char === ' '){
+            strMorze += '**********';
+        } else {
+            let charMorze = Object.keys(MORSE_TABLE)[morseValue.indexOf(char)];
+            let numStr = '';
+            for (const a of charMorze) {
+                if(a === '.'){
+                    numStr += '10';
+                }else if(a === '-'){
+                    numStr += '11';
+                }
+            }
+            while (numStr.length < 10) {
+                numStr = '0' + numStr;
+            }
+            strMorze += numStr;
+        }
+    }
+    console.log(strMorze);
+    console.log(strMorze === "10111111111111101010001110101000101011101010111111101010101011111111101110101010");
+    return strMorze;
 }
 
 module.exports = {
     decode
 }
+
+decode('17bf2596');
